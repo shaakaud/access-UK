441e17018e8f77dc0334bf603377561039b7cc4e
Udayakumar Thimma Janarthanan <udayakumar.thimma_janarthanan@alcatel-lucent.com>
2015-05-26 13:14:30 -0700

    added bin directory

---
+++ bin/bkup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+bkup_filename=$1
+bkup_dir=$HOME/bkup
+vb_win_dir=/drive_c/$bkup_dir
+bkup_file_path=$bkup_dir/$bkup_filename
+remote_bkup_server='queen2.mv.usa.alcatel.com'
+
+echo "Bakup file name is $bkup_file_path"
+
+cvs diff > $bkup_file_path
+
+if $(uname -a | grep -qi linux)
+then
+    cp $bkup_file_path $vb_win_dir/$bkup_filename
+fi
+
+echo "Local backup done as $bkup_file_path"
+
+scp $bkup_file_path $USER@$remote_bkup_server:$bkup_file_path
+
+echo "Transferred $bkup_filename to $USER@$remote_bkup_server:$bkup_file_path"
+
+echo "DONE"
