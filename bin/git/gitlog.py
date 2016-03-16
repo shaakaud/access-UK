b33f40a1fcff487914d89b05ffe638d16671a9b8
udayakut <udayakumar.thimma_janarthanan@alcatel-lucent.com>
2016-03-15 17:11:39 -0700

    added gitbkup/gitlog/gitignore/noip-update/prefix-change in tmux

---
+++ bin/git/gitlog.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import commands
+import subprocess
+import os
+import sys
+
+pr = subprocess.Popen( "/usr/bin/git logol" , cwd = os.path.dirname( '/home/udayakut/ws/mg00/panos/' ), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE )
+(out, error) = pr.communicate()
+
+
+print "Error : \n" + str(error) 
+print "out : \n" + str(out)
