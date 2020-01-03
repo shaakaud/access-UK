3219ee05197068452a886cb39cb9d88db66e1899
shaakaud <udaytj@versa-networks.com>
2020-01-03 14:49:05 -0500

    Versa - Changes as on 1/3/2020

--- bin/csc.sh
+++ bin/csc.sh
@@ -9,7 +9,7 @@ LIST_FILE=cscope.files
 
 echo -n "Generating files list .."
 (cd flexvnf && git ls-files) | \
-    egrep -i '\.([chlys](xx|pp)*|cc|hh|py|xml|yang|proto)$' | \
+    egrep -i '\.([chlys](xx|pp)*|cc|hh|py|xml|yang|proto|sh)$' | \
     sed -e '/\/CVS\//d' -e '/\/RCS\//d' -e 's/^\.\///' -r -e '/^(lib|bin|deps|objs|\.git|\.hg)\//d' | \
     sort | awk ' {print "flexvnf/" $0 } ' > $LIST_FILE
 echo "done"
@@ -22,16 +22,16 @@ cscope_pid=$!
 
 echo -n "Building ctags database as well .."
 ctags -L $LIST_FILE --c++-kinds=+p --fields=+iaS --extra=+q
-egrep ';"[[:space:]]f' tags > tags_f
-egrep ';"[[:space:]]m' tags > tags_m
-egrep ';"[[:space:]](s|t)' tags > tags_s
-egrep ';"[[:space:]](d|e)' tags > tags_d
-egrep ';"[[:space:]]v' tags > tags_v
-egrep 'iom|icc' tags_f > iom_tags_f
-egrep 'iom|icc' tags_m > iom_tags_m
-egrep 'iom|icc' tags_s > iom_tags_s
-egrep 'iom|icc' tags_d > iom_tags_d
-egrep 'iom|icc' tags_v > iom_tags_v
+#egrep ';"[[:space:]]f' tags > tags_f
+#egrep ';"[[:space:]]m' tags > tags_m
+#egrep ';"[[:space:]](s|t)' tags > tags_s
+#egrep ';"[[:space:]](d|e)' tags > tags_d
+#egrep ';"[[:space:]]v' tags > tags_v
+#egrep 'iom|icc' tags_f > iom_tags_f
+#egrep 'iom|icc' tags_m > iom_tags_m
+#egrep 'iom|icc' tags_s > iom_tags_s
+#egrep 'iom|icc' tags_d > iom_tags_d
+#egrep 'iom|icc' tags_v > iom_tags_v
 echo "done"
 
 echo -n "waiting for cscope to be over .. "
